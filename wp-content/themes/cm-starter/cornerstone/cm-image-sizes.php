2e70197f437aa947512d5d090d90b268f042113e
Luan_Nguyen <luan@cornerstone.media>
2017-03-16 10:07:01 -0400

    Just a little change

--- wp-content/themes/cm-starter/cornerstone/cm-image-sizes.php
+++ wp-content/themes/cm-starter/cornerstone/cm-image-sizes.php
@@ -4,7 +4,7 @@
 // ---
 
 // custom image sizes
-add_image_size('full-bleed', 2000, 9999, false);
+//add_image_size('full-bleed', 2000, 9999, false);
 add_image_size('container-width', 1400, 9999, false);
 add_image_size('mid-size', 900, 9999, false);
 // 3 : 1 aspect ratio for hero images
@@ -16,10 +16,13 @@ add_image_size('mid-size', 900, 9999, false);
 // to prevent uploads folder from ballooning
 function cm_remove_default_image_sizes( $sizes) {
     unset( $sizes['medium'] );
+	unset( $sizes['medium_large'] );
     unset( $sizes['large'] );
     return $sizes;
 }
 add_filter( 'intermediate_image_sizes_advanced', 'cm_remove_default_image_sizes' );
+
+// var_dump( get_intermediate_image_sizes() );
 
 
 
