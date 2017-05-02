11c479a6ab09a68b8948af328b2c1ef3382e69b2
Luan_Nguyen <luan@cornerstone.media>
2017-05-02 14:09:53 -0400

    slowly stripping away legacy page-flexible.php

--- wp-content/themes/cm-starter/page-flexible.php
+++ wp-content/themes/cm-starter/page-flexible.php
@@ -24,24 +24,6 @@ get_header(); ?>
 					// loop through the rows of data
 					while ( have_rows('content') ) : the_row(); ?>
 
-						<?php if( get_row_layout() == 'hero_banner' ): ?>
-
-							<?php
-							// ----
-							// Layout: Hero Banner -- tops of internal pages
-							$img = get_sub_field('image');
-							$bgPos = get_sub_field('bg_position') ? get_sub_field('bg_position') : 'center center';
-							$style = "background-image: url(" . $img['sizes']['1.78:1-large'] . "); background-position:" . $bgPos . ";";
-							$bgCol = empty($img) ? 'bg-pri' : '';
-							?>
-
-							<div class="hero-banner hero-banner--<?php the_sub_field('size');?> <?=$bgCol;?> ttu white tc cover relative" style="<?=$style; ?>">
-								<div class="hero-banner--content absolute center-it ttu white w-90">
-									<h2 class="fw7 lh-solid ma0 mb2"><?php the_sub_field('heading'); ?></h2>
-									<h3 class="fw3 mt0"><?php the_sub_field('sub_heading'); ?></h3>
-								</div>
-							</div>
-
 
 						<?php elseif( get_row_layout() == 'colored_text_bar' ): ?>
 
@@ -80,58 +62,6 @@ get_header(); ?>
 							</div>
 
 
-							
-
-
-						<?php elseif( get_row_layout() == 'grid_width_image' ): ?>
-
-							<?php
-							// ----
-							// Layout: Image: Full Width Img Container
-							$img = get_sub_field('image');
-							?>
-							<?php if(!empty($img)) : ?>
-								<div class="image-container row">
-									<img class="w-100" src="<?php echo $img['sizes']['container-width']; ?>" alt="<?php echo $img['alt']; ?>" />
-								</div>
-							<?php endif; ?>
-
-
-
-
-						<?php elseif( get_row_layout() == 'full_bleed_image' ): ?>
-
-							<?php
-							// ----
-							// Layout: Full Bleed Images
-							$full_width_img = get_sub_field('image');
-							$image_src = $full_width_img['sizes']['full-bleed'] != '' ? $full_width_img['sizes']['full-bleed'] : $full_width_img['url'];
-							?>
-
-							<?php if(!empty($full_width_img)) : ?>
-								<div class="full-bleed-image">
-									<img class="lazy full-width-img" src="<?=$image_src;?>" alt="<?php echo $full_width_img['alt']; ?>" />
-								</div>
-							<?php endif; ?>
-
-
-					
-
-
-						<?php elseif( get_row_layout() == 'wysiwyg' ): ?>
-
-							<?php
-							// ----
-							// Layout: WYSIWYG
-
-							$elem_classes = get_sub_field('constrained_content') ? 'constrained-content' : '';
-							$elem_classes = get_sub_field('reduce_padding') ? $elem_classes . ' reduce' : $elem_classes;
-							?>
-							<?php if (get_sub_field('bg_color')) { echo '<div class="bg-'. get_sub_field('bg_color') . '">'; } ?>
-								<div class="row content-section wysiwyg <?=$elem_classes;?>">
-									<?php the_sub_field('content'); ?>
-								</div>
-							<?php if (get_sub_field('bg_color')) { echo '</div>'; } ?>
 
 
 
@@ -151,28 +81,10 @@ get_header(); ?>
 							</div>
 
 
-
-
-						<?php elseif( get_row_layout() == 'form' ): ?>
-
-							<?php
-							// ----
-							// Layout: Form
-							$shortcode = get_sub_field('shortcode');
-							?>
-						
-							<div class="row pa3 form-module mb4">
-								<?php 
-									if ($shortcode) {
-										echo do_shortcode($shortcode);
-									}
-								?>
-							</div>
 							
 
 
-							
-
+					
 
 
 
