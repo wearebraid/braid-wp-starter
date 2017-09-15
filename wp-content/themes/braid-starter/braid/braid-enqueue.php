c51ed24dbd3a1e274af951e05297113816570a8b
Chris <chris@oxdeer.com>
2017-09-15 12:07:16 -0400

    updating gulp file and wordpress enqueues

--- wp-content/themes/braid-starter/braid/braid-enqueue.php
+++ wp-content/themes/braid-starter/braid/braid-enqueue.php
@@ -5,8 +5,8 @@
 
 function braid_starter_scripts() {
     // each time the below files are saved, the version fingerprint changes to bust cache
-    $clientCSSversion = filemtime(get_template_directory() . '/dist/app.min.css');
-    $clientJSversion = filemtime(get_template_directory() . '/dist/app.min.js');
+    $clientCSSversion = filemtime(get_template_directory() . '/dist/css/app.min.css');
+    $clientJSversion = filemtime(get_template_directory() . '/dist/js/app.min.js');
     $styleCSSVersion = filemtime(get_template_directory() . '/style.css');
 
     // default style.css which has a normalize
@@ -17,10 +17,10 @@ function braid_starter_scripts() {
     // skip-link-focus.js is included in gulp bundle for performance
 
 	// Main Client CSS
-	wp_enqueue_style( 'braid-starter-app-style', get_template_directory_uri() . '/dist/app.min.css', [], $clientCSSversion);
+	wp_enqueue_style( 'braid-starter-app-style', get_template_directory_uri() . '/dist/css/app.min.css', [], $clientCSSversion);
 
 	// Main Client JS
-	wp_enqueue_script( 'braid-starter-app-js', get_template_directory_uri() . '/dist/app.min.js', ['jquery'], $clientJSversion, true );
+	wp_enqueue_script( 'braid-starter-app-js', get_template_directory_uri() . '/dist/js/app.min.js', ['jquery'], $clientJSversion, true );
 
     // Font Awesome
     wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/8ef1cdd67b.js', [], false);
--- wp-content/themes/braid-starter/gulpfile.js
+++ wp-content/themes/braid-starter/gulpfile.js
@@ -1,139 +1,52 @@
-// gulp packages
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var clean = require('gulp-clean');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var concatCss = require('gulp-concat-css');
-var livereload = require('gulp-livereload');
-var notify = require("gulp-notify");
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
+'use strict';
+
+var proxy_url = 'localhost';  // REPLACE THIS WITH YOUR LOCAL DEV URL
+var proxy_secure = false;     // SET PROXY TO USE HTTPS OR HTTP
+
+// PUT ALL FONT FILES USED HERE TO BE COPIED INTO BUILD DIRECTORY
+var fonts = [
+  './node_modules/font-awesome/fonts/*',
+];
+
+var appJS = ['./lib/js/script.js'];
+
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+/* YOU SHOULDN'T NEED TO MODIFY ANYTHING BELOW HERE UNLESS YOU WANT TO ADD NEW FUNCTIONALITY */
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
 
 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserify = require('gulp-browserify'),
+    browserSync = require('browser-sync'),
+    cssnano = require('gulp-cssnano'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    notify = require("gulp-notify"),
+    concat = require('gulp-concat');
 
 
-// ----
-// gulp utilities
-// ----
-
-// on error function for plumber
 var onError = function (err) {
     console.log(err);
     this.emit('end');
 };
 
-
-
-// Livereload
-gulp.task('reload', function () {
-    livereload({
-        start: true,
-        reloadPage: ''
-    });
-});
-
-
-
-
-// remove all files from the dist folder
-gulp.task('clean', function () {
-    return gulp.src('./dist/**/*', {read: false})
-        .pipe(clean());
-});
-
-
-
-
-// build
-gulp.task('build', ['js', 'css']);
-
-
-
-
-// watch task
-gulp.task('watch', ['reload'], function () {
-    livereload.listen();
-    gulp.watch('./lib/scss/**/*.scss', ['css']);
-    gulp.watch('./lib/js/*.js', ['js']);
-});
-
-
-
-
-// default task (run with "gulp" command)
-gulp.task('default', ['build', 'watch']);
-
-
-
-// ----
-// gulp javascript section
-// ----
-
-// app.js files -- where almost all JS files should go
-var appJS = [
-    './lib/js/vendor/jquery.lazy.js',
-    //'./lib/js/vendor/slick.js', // Slick also requires CSS. Uncomment it at lib/scss/style.scss
-    './js/skip-link-focus-fix.js',
-    './lib/js/script.js'
-];
-
-
-// modular JS processing tasks so we can run the same functions whether head or app js
-function processJS(js, concatName) {
-    return gulp.src(js)
-        .pipe(plumber())
-        .pipe(concat(concatName))
-        .pipe(gulp.dest('./dist/'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/'))
-        .pipe(livereload())
-        .pipe(notify({
-            title: "JS task complete",
-            message: concatName + " generated and minified",
-            onLast: true,
-            sound: true
-        }
-    ));
-}
-
-
-// executes all js tasks
-gulp.task('js', ['js-app']);
-
-
-
-// body JS -- all the rest of the JS that goes right above the closing </body> tag.
-gulp.task('js-app', function () {
-    return processJS(appJS, 'app.js');
-});
-
-
-
-
-
-// ----
-// gulp CSS section
-// ----
-
-var globalCss = [
-    './lib/scss/*.scss',
-];
-
-
-
-// alias for "sass" task
-gulp.task('css', ['sass']);
-
-
-
-// modular CSS processing fimctopm so we can run multiple tasks through
-function processCss(css, concatName) {
-    return gulp.src(css)
+ 
+// COMPILE THEME SASS FILE
+gulp.task('sass', function () {
+    return gulp.src('./lib/scss/style.scss')
+        .pipe(sourcemaps.init())
         .pipe(plumber({
             errorHandler: onError
         }))
@@ -146,26 +59,74 @@ function processCss(css, concatName) {
             browsers: ['> 1%', 'Safari >= 8'],
             cascade: false
         }))
-        .pipe(concatCss(concatName))
-        .pipe(gulp.dest('./dist'))
         .pipe(cleanCSS({debug: true, advanced: false}, function(details) {
             console.log(details.name + ' size before minifying: ' + details.stats.originalSize);
             console.log(details.name + ' size after: ' + details.stats.minifiedSize);
         }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'))
+        .pipe(rename({
+            basename: "app",
+            suffix: ".min",
+            extname: ".css"
+          }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(notify({
-            title: "sass task complete",
-            message: concatName + " generated",
+            title: "Sass compiled successfully!",
             onLast: true,
             sound: true
         }))
-        .pipe(livereload());
-}
-
-
-
-// scss compile
-gulp.task('sass', function () {
-    return processCss(globalCss, 'app.css');
 });
+
+
+// COPY ALL FONTS INTO FONT BUILD DIRECTORY
+gulp.task('fonts', function () {
+  return gulp.src(fonts)
+    .pipe(gulp.dest('./dist/fonts/'));
+});
+
+
+// MINIFY THEME JS FILE
+gulp.task('script', function() {
+  return gulp.src(appJS)
+    .pipe(plumber())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.init())
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : false
+    }))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(notify({
+        title: "Scripts compiled successfully!",
+        onLast: true,
+        sound: true
+    }))
+});
+
+
+// BROWSERSYNC AND WATCH
+gulp.task('watch', function () {
+  var files = [
+    './dist/css/*.css', 
+    './dist/js/*.js',
+    '**/*.php',
+    'images/**/*.{png,jpg,gif,svg,webp}',
+  ];
+
+  browserSync.init(files, { 
+    https: proxy_secure,
+    proxy: {
+        target: proxy_url
+    },
+  });
+
+  gulp.watch('lib/scss/**/*.scss', ['sass']);
+  gulp.watch('lib/js/**/*.js', ['script']);
+});
+
+
+gulp.task('default', ['production','watch']);
+gulp.task('production', ['fonts','script','sass']);
