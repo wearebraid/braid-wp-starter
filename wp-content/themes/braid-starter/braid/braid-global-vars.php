4a36a1d59012f9d87e0f17bb58a3cf79faa8d3a6
Chris Geelhoed <cgeelhoed@wearebraid.com>
2018-02-19 17:07:53 -0800

    Check for ACF before declaring globals

--- wp-content/themes/braid-starter/braid/braid-global-vars.php
+++ wp-content/themes/braid-starter/braid/braid-global-vars.php
@@ -8,12 +8,15 @@
 $uploads = wp_upload_dir()['baseurl'];
 $root = get_template_directory_uri();
 $img_root = $root . "/lib/img";
-$genPhone = get_field('phone', 'option');
-$genPhoneHref = 'tel:' . str_replace(["-", ".", "(", ")"], "", $genPhone);
-$genFax = get_field('fax', 'option');
-$genFaxHref = 'fax:' . str_replace(["-", "."], "", $genFax);
-$genEmail = get_field('email', 'option');
-$genAddress = get_field('address', 'option');
-$addressGoogleFormat = "https://maps.google.com/?daddr=" . str_replace([", ", " "], "+", $genAddress);
+
+if( class_exists('acf') ) {
+    $genPhone = get_field('phone', 'option');
+    $genPhoneHref = 'tel:' . str_replace(["-", ".", "(", ")"], "", $genPhone);
+    $genFax = get_field('fax', 'option');
+    $genFaxHref = 'fax:' . str_replace(["-", "."], "", $genFax);
+    $genEmail = get_field('email', 'option');
+    $genAddress = get_field('address', 'option');
+    $addressGoogleFormat = "https://maps.google.com/?daddr=" . str_replace([", ", " "], "+", $genAddress);    
+}
 
 ?>
