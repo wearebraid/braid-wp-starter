b7eeae3ba5e79c30abbe81e982c3b095a0167cc8
Chris Geelhoed <cgeelhoed@wearebraid.com>
2019-03-28 18:42:57 -0700

    Adds basic babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -129,6 +129,16 @@ module.exports = (env, argv) => ({
       {
         test: /\.vue$/,
         loader: 'vue-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
