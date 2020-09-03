70be7caea56fe5617f6e86349fb65bcb91c5dcdb
Chris Adams <chrisladams@users.noreply.github.com>
2020-09-03 10:20:05 -0400

    adding filter for proper removal of script and css

--- braid/template-parts/flexible-wysiwyg.php
+++ braid/template-parts/flexible-wysiwyg.php
@@ -7,6 +7,6 @@ $elem_classes = get_sub_field('reduce_padding') ? $elem_classes . ' reduce' : $e
 ?>
 <?php if (get_sub_field('bg_color')) { echo '<div class="bg-'. get_sub_field('bg_color') . '">'; } ?>
     <div class="row content-section <?=$elem_classes;?>">
-        <?php the_sub_field('content'); ?>
+        <?php echo apply_filters('the_content', get_sub_field('content', false, false)); ?>
     </div>
 <?php if (get_sub_field('bg_color')) { echo '</div>'; } ?>
